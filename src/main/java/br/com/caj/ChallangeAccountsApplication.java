a9996e63bc319a36d7c95e7114086110b748ebdf
Josiel Sousa <josiel.psousa@gmail.com>
2021-01-13 17:57:56 -0300

    feat: add domain dependency.

--- src/main/java/br/com/caj/ChallangeAccountsApplication.java
+++ src/main/java/br/com/caj/ChallangeAccountsApplication.java
@@ -2,8 +2,10 @@ package br.com.caj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.caj.domain")
 public class ChallangeAccountsApplication {
 
 	public static void main(String[] args) {
--- src/test/java/br/com/caj/ChallangeAccountsApplicationTests.java
+++ src/test/java/br/com/caj/ChallangeAccountsApplicationTests.java
@@ -1,8 +1,11 @@
 package br.com.caj;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ChallangeAccountsApplicationTests {
 
