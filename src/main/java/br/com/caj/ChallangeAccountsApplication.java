9d9e27ca01b29db639cddff5d0cfa024e5f11e6b
Josiel Sousa <josiel.psousa@gmail.com>
2021-01-13 14:52:21 -0300

    feat: initialize repo to challange

---
+++ src/main/java/br/com/caj/ChallangeAccountsApplication.java
@@ -0,0 +1,13 @@
+package br.com.caj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallangeAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallangeAccountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/caj/ChallangeAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallangeAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
