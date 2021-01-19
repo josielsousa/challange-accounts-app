b13f505b328e541b57196289ca03bcebf0c48b7d
Josiel Sousa <josiel.psousa@gmail.com>
2021-01-19 10:55:16 -0300

    feat: add endpoint to get account by uuid.

--- src/main/java/br/com/caj/entrypoint/AccountController.java
+++ src/main/java/br/com/caj/entrypoint/AccountController.java
@@ -7,11 +7,14 @@ import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.caj.config.http.ErrorMessage;
+import br.com.caj.domain.entity.Account;
 import br.com.caj.domain.usecase.AccountUseCase;
 import br.com.caj.domain.usecase.exception.AccountException;
+import br.com.caj.domain.usecase.exception.AccountNotFoundException;
 import br.com.caj.entrypoint.model.AccountModel;
 
 /**
@@ -37,16 +40,29 @@ public final class AccountController implements Serializable {
   @GetMapping("/accounts")
   public ResponseEntity<?> listAccounts() {
     try {
-      final Set<AccountModel> accounts = accountUseCase.getAllAccounts()
-        .stream()
-        .map(AccountModel::fromDomain)
-        .collect(Collectors.toSet());
+      final Set<AccountModel> accounts = accountUseCase.getAllAccounts().stream().map(AccountModel::fromDomain)
+          .collect(Collectors.toSet());
 
       return ResponseEntity.ok(accounts);
     } catch (AccountException e) {
       return ResponseEntity.badRequest().body(new ErrorMessage(e.getMessage()));
     } catch (Exception e) {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorMessage(e.getMessage()));
-    }  
+    }
+  }
+
+  /**
+   * Return an account by uuid.
+   */
+  @GetMapping("/accounts/{uuid}")
+  public ResponseEntity<?> getAccount(@PathVariable("uuid") final String uuid) {
+    try {
+      final Account account = accountUseCase.getAccount(uuid);
+      return ResponseEntity.ok(account);
+    } catch (AccountNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorMessage(e.getMessage()));
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorMessage(e.getMessage()));
+    }
   }
 }
