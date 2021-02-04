156701536b391d9d211bf1350fa4f3b8c004b69f
Josiel Sousa <josiel.psousa@gmail.com>
2021-02-04 09:59:26 -0300

    chore: adjust custom data config mongo profile.

--- src/main/java/br/com/caj/config/mongo/CustomPrepareDataConfig.java
+++ src/main/java/br/com/caj/config/mongo/CustomPrepareDataConfig.java
@@ -6,25 +6,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import br.com.caj.dataprovider.mongo.repository.AccountRepository;
+import br.com.caj.dataprovider.mongo.repository.TransferRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
 @Profile("mongo")
+@RequiredArgsConstructor
 public class CustomPrepareDataConfig implements ApplicationRunner {
 
   private final AccountRepository accountRepository;
-
-  /**
-   * Default constructor.
-   */
-  public CustomPrepareDataConfig(AccountRepository accountRepository) {
-    this.accountRepository = accountRepository;
-  }
+  private final TransferRepository transferRepository;
 
   @Override
   public void run(ApplicationArguments args) throws Exception {
     if (accountRepository.count() > 0) {
+      return;
+    }
+
+    if (transferRepository.count() > 0) {
       return;
     }
 
